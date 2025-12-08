4ec437acfaccdc51aad657878de9f77060d248cc
Himanshuu <2203himanshusingh@gmail.com>
2025-12-09 04:36:59 +0530

    dkeho

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,15 @@ const Header = () => {
     <div>
       <header className="header">
         <link rel="icon" href={favicon} />
-        <div className="logo-container">
+        <div
+          className="logo-container"
+          onClick={() => window.location.reload()}
+          style={{ cursor: "pointer" }} // cursor pointer to indicate clickable
+        >
           <img src={logo} alt="Brand Logo" className="logo-img" />
           <h1 className="logo-text">HANKET</h1>
         </div>
+
         <button
           className="btn header-btn"
           onClick={() => (window.location.href = "tel:+919876543210")}
