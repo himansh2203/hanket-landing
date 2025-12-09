f0491d513cbb66b137f22330cf6fe8d665d7b08b
Himanshuu <2203himanshusingh@gmail.com>
2025-12-09 11:56:11 +0530

    head mobe13

--- src/components/Circular.jsx
+++ src/components/Circular.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import img1 from "../assets/hanket_image.jpg";
-import HassleFreeSetup from "./HassleFreeSetup";
-import MarketplaceLaunch from "./MarketPlaceLaunch";
-import BrandIdentity from "./BrandIdentity";
-import FreePhotoshoot from "./FreePhotoShoot";
+import photo1 from "../assets/hanket_image.jpg";
+import photo2 from "../assets/hanket_image.jpg";
+import photo3 from "../assets/hanket_image.jpg";
+import photo4 from "../assets/hanket_image.jpg";
 
 const Circular = () => {
   const scrollToSection = (id) => {
@@ -21,36 +21,40 @@ const Circular = () => {
           <img src={img1} alt="center" />
         </div>
 
-        {/* RECT 1 → scroll to FreePhotoshoot */}
+        {/* RECT 1 → Free Photoshoot */}
         <div
-          className="rect rect1"
+          className="rect rect1 card"
           onClick={() => scrollToSection("photoshoot-sec")}
         >
-          <FreePhotoshoot />
+          <img src={photo1} alt="Free Photoshoot" />
+          <h3>Free Photoshoot</h3>
         </div>
 
-        {/* RECT 2 → scroll to Brand Identity */}
+        {/* RECT 2 → Brand Identity */}
         <div
-          className="rect rect2"
+          className="rect rect2 card"
           onClick={() => scrollToSection("brand-sec")}
         >
-          <BrandIdentity />
+          <img src={photo2} alt="Brand Identity" />
+          <h3>Brand Identity</h3>
         </div>
 
-        {/* RECT 3 → scroll to Marketplace Launch */}
+        {/* RECT 3 → Marketplace Launch */}
         <div
-          className="rect rect3"
+          className="rect rect3 card"
           onClick={() => scrollToSection("market-sec")}
         >
-          <MarketplaceLaunch />
+          <img src={photo3} alt="Marketplace Launch" />
+          <h3>Marketplace Launch</h3>
         </div>
 
-        {/* RECT 4 → scroll to Hassle Free Setup */}
+        {/* RECT 4 → Hassle Free Setup */}
         <div
-          className="rect rect4"
+          className="rect rect4 card"
           onClick={() => scrollToSection("setup-sec")}
         >
-          <HassleFreeSetup />
+          <img src={photo4} alt="Hassle Free Setup" />
+          <h3>Hassle Free Setup</h3>
         </div>
       </div>
     </section>
