3039662fa912c16099752ebd7090c8350446d5b4
Himanshuu <2203himanshusingh@gmail.com>
2025-12-09 12:37:05 +0530

    removed circular iamgr

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 const ContactForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault(); // page reload rokhne ke liye
+    alert("Your form has been submitted!");
+
+    // Formsubmit.co pe form submit karna
+    e.target.submit();
+
+    // Redirect to another website after submission
+    window.location.href = "https://hanket-landing.vercel.app/"; // replace with your desired URL
+  };
+
   return (
     <section className="contact">
       <h2 className="section-title">Contact Us</h2>
@@ -9,16 +20,15 @@ const ContactForm = () => {
       </p>
       <form
         className="contact-form"
-        action="https://formsubmit.co/hanketstudio@gmail.com"
+        action="https://formsubmit.co/333himansusingh@gmail.com"
         method="POST"
+        onSubmit={handleSubmit}
       >
         {/* Important hidden inputs */}
         <input type="hidden" name="_captcha" value="false" />
         <input type="hidden" name="_template" value="box" />
         <input type="hidden" name="_subject" value="New Contact Form Message" />
-        <input type="hidden" name="_next" value="http://localhost:5173/" />
 
-        {/* User Inputs */}
         <input type="text" name="name" placeholder="Your Name" required />
         <input type="email" name="email" placeholder="Your Email" required />
         <textarea
